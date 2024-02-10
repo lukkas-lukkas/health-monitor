62ff27bd4f7be877e5906f8f1ae7cc41c224a9f1
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-02-10 09:32:47 -0300

    create endpoint

---
+++ application/src/UI/Http/Controllers/StoreHealthDataController.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace HealthMonitor\UI\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class StoreHealthDataController extends Controller
+{
+    public function __invoke(string $userId, Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'started_at' => ['required', 'date_format:Y-m-d\TH:i:s'],
+            'finished_at' => ['required', 'date_format:Y-m-d\TH:i:s'],
+            'avg_bpm' => ['required', 'integer'],
+            'steps_total' => ['required', 'integer'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->all(), 422);
+        }
+        $data = $validator->validated();
+        $data['user_id'] = $userId;
+
+        return response()->json($data);
+    }
+}
