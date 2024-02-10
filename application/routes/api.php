26d1a117591b3a348781e85fcda55f8c211da0fc
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-02-10 10:30:35 -0300

    create endpoint and controller

--- application/routes/api.php
+++ application/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use HealthMonitor\UI\Http\Controllers\GetHealthDataByUserController;
 use HealthMonitor\UI\Http\Controllers\StoreHealthDataController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/v1/users/{userID}/health-data', GetHealthDataByUserController::class);
 Route::post('/v1/users/{userID}/health-data', StoreHealthDataController::class);
